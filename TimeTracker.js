408af200414532cbb89079b74434ad9432a4bdbc
Sofeo <sofeous@gmail.com>
2020-01-14 15:09:53 +0100

    Made the timer able to start and stop it only counts in full seconds

--- TimeTracker.js
+++ TimeTracker.js
@@ -2,4 +2,21 @@
 let start = document.getElementById('start');
 let stop = document.getElementById('stop');
 let clear = document.getElementById('clear');
+let timehold = document.getElementById('time');
 
+let time = 0;
+let intrval;
+
+start.addEventListener("click", startTimer);
+stop.addEventListener("click", stopTimer);
+
+function startTimer() {
+    interval = setInterval(function(){
+        time++;
+        timehold.innerHTML = time;
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(interval)
+}
