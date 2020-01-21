72bf86ea0501a71927d9dbbfca4ed35a9ac7a293
Sofeo <sofeous@gmail.com>
2020-01-22 00:44:47 +0100

    added laps made a button that mesures the laps

--- TimeTracker.js
+++ TimeTracker.js
@@ -2,10 +2,13 @@
 let start = document.getElementById('start');
 let reset = document.getElementById('reset');
 let timehold = document.getElementById('time');
+let laphold = document.getElementById('lapHold');
+
 
 let time = 0;
 let intrval;
 
+document.getElementById('lap').addEventListener("click", goLap);
 start.addEventListener("click", startTimer);
 reset.addEventListener("click", resetTimer);
 
@@ -31,4 +34,17 @@ function resetTimer() {
     stopTimer();
     time = 0;
     timehold.innerHTML = time;
+    laphold.innerHTML = "";
+}
+
+let lastLap = 0;
+function goLap() {
+    let p = document.createElement("p");
+    let text;
+
+    text = document.createTextNode((time / 100) - lastLap);
+    lastLap = time / 100;
+  
+    p.appendChild(text);
+    laphold.prepend(p);
 }
